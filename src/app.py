af2293149372c318eb5bd78cdca2bd6cba827f38
Matti Stenvall <matti.stenvall@gmail.com>
2023-12-03 18:46:28 +0200

    modified character related views to use flask blueprint.

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from routes.character_routes import character_routes
 from routes.map_routes import map_routes
 from blueprints.map import map_blueprint
 from blueprints.auth import auth_blueprint
+from blueprints.character import character_blueprint
 from db.db import db
 from models.user_model import User
 from models.character_model import Character
@@ -19,9 +20,9 @@ app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
-app.register_blueprint(map_blueprint, url_prefix='/map')
-app.register_blueprint(auth_blueprint, url_prefix='/')
-character_routes(app)
+app.register_blueprint(map_blueprint)
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(character_blueprint)
 
 if __name__ == '__main__':
         app.run(debug=True)
