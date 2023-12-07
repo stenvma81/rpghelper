c2f52badc053ebb15a8f85346773548af1c42bce
Matti Stenvall <matti.stenvall@gmail.com>
2023-12-07 18:39:50 +0200

    fixed a bug with blueprint static folders

--- src/blueprints/map/__init__.py
+++ src/blueprints/map/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 map_blueprint = Blueprint('map_blueprint', __name__,
                           template_folder='templates',
-                          static_folder='static')
+                          static_folder='static',
+                          static_url_path='/blueprints/map/static)')
 
 from .map_routes import *
